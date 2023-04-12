f30a8e492e5f52470107e6024ecd1df56a07c919
ayeagle <alexyeagle@gmail.com>
2023-04-11 19:29:25 -0700

    stuff

--- flytrap/src/app.js
+++ flytrap/src/app.js
@@ -15,6 +15,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 console.log("app has loaded");
 
 // import './app.css';
+//ksdjhasjd
 
 export default function App() {
   // const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } =
