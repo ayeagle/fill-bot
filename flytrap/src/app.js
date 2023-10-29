9973a319ed157e8b2547f9f4959eb01f0519fc9e
Alexander Yeagle <AlexanderYeagle@MacBook-Pro.local>
2023-10-29 08:27:36 -0700

    cleaning up some comments

--- flytrap/src/app.js
+++ flytrap/src/app.js
@@ -14,50 +14,12 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 console.log("app has loaded");
 
-// import './app.css';
-//ksdjhasjd
-
 export default function App() {
-  // const { isLoading, isAuthenticated, error, user, loginWithRedirect, logout } =
-  //   useAuth0();
-
-  // if (isLoading) {
-  //   return <div>Loading...</div>;
-  // }
-  // if (error) {
-  //   return <div>Oops... {error.message}</div>;
-  // }
-
-  // if (isAuthenticated) {
     return (
-      // <div>
-      //   Hello {user.name}{" "}
-      //   <button onClick={() => logout({ returnTo: window.location.origin })}>
-      //     Log out
-      //   </button>
-      // </div>
 
       <div id="app" className={styles.main_container}>
-        {/* <CoreApp {...pageProps}  /> */}
-        {/* <Component {...pageProps} /> */}
         <Component {...pageProps} />
-
-        {/* <NavBar /> */}
       </div>
     )
-    
-  //   ;
-  // } else {
-  //   return <button onClick={() => loginWithRedirect()}>Log in</button>;
-  // }
 
-  // return (
-  //   <div id="app" className={styles.main_container}>
-  //     {/* <CoreApp {...pageProps}  /> */}
-  //     {/* <Component {...pageProps} /> */}
-  //     <Component {...pageProps} />
-
-  //     {/* <NavBar /> */}
-  //   </div>
-  // );
 }
